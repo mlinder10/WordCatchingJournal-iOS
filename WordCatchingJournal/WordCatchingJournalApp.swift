2b8f3f77d3c8f6fbbb12bb37a47b706c6b7781a8
Matt Linder <linder2015@outlook.com>
2024-08-16 00:46:10 -0400

    Initial Commit

---
+++ WordCatchingJournal/WordCatchingJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCatchingJournalApp.swift
+//  WordCatchingJournal
+//
+//  Created by Matt Linder on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WordCatchingJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
